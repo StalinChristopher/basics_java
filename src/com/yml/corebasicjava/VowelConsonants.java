2fbbf29a4083ab0654620a42db274d263c5ed6a8
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-08-24 23:39:06 +0530

    Added logic to find if the character is a vowel or consonant

---
+++ src/com/yml/corebasicjava/VowelConsonants.java
@@ -0,0 +1,17 @@
+package com.yml.corebasicjava;
+import java.util.*;
+
+public class VowelConsonants {
+	public static void vowelConsonantsMethod() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the character");
+		char c = in.next().charAt(0);
+		
+		if( c=='a' || c=='e' || c=='i'|| c=='o' || c=='u') {
+			System.out.println(c+" is a vowel");
+		}
+		else {
+			System.out.println(c+" is a consonant");
+		}
+	}
+}
